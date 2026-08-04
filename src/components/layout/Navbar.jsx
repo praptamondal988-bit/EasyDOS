ed0570e895b159e57b55d7c24dd4872c54dbe7b4
praptamondal988-bit <praptamondal988@gmail.com>
2026-08-04 11:09:37 +0000

    Fixed mobile navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -6,37 +6,33 @@ function Navbar() {
 
   return (
     <nav className="navbar">
-
       <div
         className="navbar-logo"
         onClick={() => navigate("/")}
       >
         <div className="logo-box">E</div>
 
-        <div>
+        <div className="logo-text">
           <h2>EasyDOS</h2>
           <span>Learn. Practice. Grow.</span>
         </div>
       </div>
 
       <div className="navbar-menu">
-
         <button onClick={() => navigate("/")}>
-          🏠 Home
+          Home
         </button>
 
         <button onClick={() => navigate("/class/9")}>
-          📘 Class 9
+          Class 9
         </button>
 
         <button onClick={() => navigate("/class/10")}>
-          📗 Class 10
+          Class 10
         </button>
-
       </div>
 
       <div className="navbar-actions">
-
         <button className="search-btn">
           🔍
         </button>
@@ -44,9 +40,7 @@ function Navbar() {
         <button className="login-btn">
           Login
         </button>
-
       </div>
-
     </nav>
   );
 }
